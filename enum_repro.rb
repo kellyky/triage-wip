7d0cf35e36130e1ca8b4da81d0d688089e35d3f2
Kelly Popko <kelpopko@gmail.com>
2024-10-22 10:43:19 -0500

    Move notes from .rb file to README

--- enum_repro.rb
+++ enum_repro.rb
@@ -31,21 +31,3 @@ p enum.each { |item| 42 } # => [42, 42, 42]
 
 # No offense
 p enum.each { |item| item >= 2 } #=> [2, 3]
-
-#############   NOTES ############
-# Observations:
-#   - Behavior in issue, as reported, was reproduced
-
-# Questions:
-#   - Is the behavior expected? (Based on docs, code)
-#   - If NOT expected, bug - then we can try to fix
-#   - if IS expected, not a bug, but raises the question - should it be changed?
-#   - Do we understand what is meant by 'void context' well enough to comment or fix?
- 
-# Patterns
-# Whether enum is [].map, [].filter, [].select:
-# - Offenses occur when when nothing is done in the block (nothing more than a value or block variable)
-# - NO Offenses occur when something is done in the block
-
-# Link to issue: https://github.com/rubocop/rubocop/issues/6963
-# Rubocop documentation on Lint/Void - https://www.rubydoc.info/gems/rubocop/RuboCop/Cop/Lint/Void
